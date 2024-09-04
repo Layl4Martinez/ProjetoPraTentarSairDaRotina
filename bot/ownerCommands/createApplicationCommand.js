85f041e66e2bdb06ad218c1f164971aff92f8e61
Layl4Martinez <laylamartinez@duck.com>
2024-09-04 14:59:11 +0000

    commit

--- bot/ownerCommands/createApplicationCommand.js
+++ bot/ownerCommands/createApplicationCommand.js
@@ -5,6 +5,12 @@ module.exports = {
   $clearReactions[$channelID;$messageID;🕑]
   $createApplicationCommand[global;stats;description...;true;true;slash]
   $createApplicationCommand[global;daily;description...;true;true;slash]
+  $createApplicationCommand[global;atm;description...;true;false;slash;[{
+    "name": "usuário",
+    "description": "description...",
+    "required": false,
+    "type": 6
+}]]
   $addCmdReactions[🕑]
   $onlyForIDs[$clientOwnerIDs[;];❌ Acesso Negado. {reply:$messageID:true}]
   `
---
+++ bot/slashCommands/economy/atm.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'atm',
+    type: 'interaction',
+    prototype: 'slash',
+    code: `
+    $interactionReply[$if[$get[user]==$authorID;Você;<@$get[user]>] tem **$getGlobalUserVar[coins;$get[user];user] coins** 🪙]
+    $let[user;$findMember[$slashOption[usuário];true;$guildID]]
+    `
+}
+
+// $interactionData[customId]
